5ad18d933c4ed757688d11e86ead6a84ae6eeb1b
vflashm <vflashm@gmail.com>
2015-03-01 20:42:16 -0500

    first partially working code

--- src/TetrisTime.c
+++ src/TetrisTime.c
@@ -1,5 +1,125 @@
 #include <pebble.h>
+#include "digit.h"
+  
+#define ANIMATION_TIMEOUT_MS 100
+  
+#define FIELD_WIDTH 16
+#define FIELD_HEIGHT 16
+#define CELL_SIZE 3
+#define CELL_SPACING 1
+  
+  
+bool s_field[FIELD_WIDTH][FIELD_HEIGHT];
+  
+static Window *s_window;
+static Layer *s_layer;
+
+
+static void layer_draw(Layer *layer, GContext *ctx) {
+    //GRect bounds = layer_get_bounds(layer);  
+  
+    GRect rect;
+    rect.size.h = CELL_SIZE;
+    rect.size.w = CELL_SIZE;
+  
+    for (int i = 0; i < FIELD_WIDTH; ++i) {
+        for (int j = 0; j < FIELD_HEIGHT; ++j) {      
+            rect.origin.x = CELL_SPACING + i * (CELL_SIZE + CELL_SPACING);
+            rect.origin.y = CELL_SPACING + j * (CELL_SIZE + CELL_SPACING);
+            GColor color = s_field[i][j] ? GColorBlack : GColorWhite;
+            graphics_context_set_fill_color(ctx, color);
+            graphics_fill_rect(ctx, rect, 0, GCornerNone);
+        }
+    }  
+}
+
+static void update_time() {
+    for (int i = 0; i < FIELD_WIDTH; ++i) {
+        for (int j = 0; j < FIELD_HEIGHT; ++j) {
+            s_field[i][j] = rand() % 2 == 0;
+        }
+    }
+    layer_mark_dirty(s_layer);
+  
+    //window_get_root_layer(window);
+  
+    /*
+    // Get a tm structure
+    time_t temp = time(NULL); 
+    struct tm *tick_time = localtime(&temp);
+
+    // Create a long-lived buffer
+    static char buffer[] = "00:00";
+
+    // Write the current hours and minutes into the buffer
+    if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    } else {
+    //Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    }
+
+    // Display this time on the TextLayer
+    text_layer_set_text(s_time_layer, buffer);
+    */
+}
+
+static void process_animation(void* data) {
+    update_time();
+    app_timer_register(ANIMATION_TIMEOUT_MS, process_animation, NULL);
+}
+
+static void main_window_load(Window *window) {
+    // Create time TextLayer
+    s_layer = window_get_root_layer(window);
+    layer_set_update_proc(s_layer, layer_draw);
+  
+    // Make sure the time is displayed from the start
+    update_time();
+  
+}
+
+static void main_window_unload(Window *window) {
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+  
+static void init() {
+#if USE_RAW_DIGITS == 1
+    for (int i = 0; i < 1; ++i) {
+    if (!parse_raw_digit(&s_digits[i], &s_raw_digits[i])) {
+    return;
+}
+}
+#endif 
+  
+    // Create main Window element and assign to pointer
+    s_window = window_create();
+
+    // Set handlers to manage the elements inside the Window
+    window_set_window_handlers(s_window, (WindowHandlers) {
+            .load = main_window_load,
+                .unload = main_window_unload
+                });
+
+    // Show the Window on the watch, with animated=true
+    window_stack_push(s_window, true);
+  
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    app_timer_register(ANIMATION_TIMEOUT_MS, process_animation, NULL);
+}
+
+static void deinit() {
+    // Destroy Window
+    window_destroy(s_window);
+}
 
 int main(void) {
-  app_event_loop();
+    init();
+    app_event_loop();
+    deinit();
 }
---
+++ src/digit.h
@@ -0,0 +1,133 @@
+#include "pebble.h"
+#include "tetrimino.h"
+  
+#define DIGIT_MAX_TETRIMINOS 32
+#define DIGIT_WIDTH 6
+#define DIGIT_HEIGHT 10
+#define USE_RAW_DIGITS 1
+  
+typedef struct {
+    char letter;
+    int rotation;
+    int x;
+    int y;
+} TetriminoPos;
+  
+typedef struct {
+    TetriminoPos tetriminos[DIGIT_MAX_TETRIMINOS];
+} DigitDef;
+
+static DigitDef s_digits[10];
+
+#if USE_RAW_DIGITS == 1
+  
+typedef char RawDigit[DIGIT_HEIGHT][DIGIT_WIDTH];
+
+static RawDigit s_raw_digits[1] = {
+    {"tzzjjj",
+     "ttzzsj",
+     "tT  ss",
+     "TT  ts",
+     "sT  tt",
+     "ss  tz",
+     "is  zz",
+     "it  zj",
+     "ittssj",
+     "itssjj"}
+};
+
+static int match_mask(RawDigit* raw, const TetriminoMask* mask, int pos_x, int pos_y, char letter, int clear) {
+    for (int mask_y = 0; mask_y < TETRIMINO_MASK_SIZE; ++mask_y) {
+        for (int mask_x = 0; mask_x < TETRIMINO_MASK_SIZE; ++mask_x) {
+            if (!(*mask)[mask_y][mask_x]) {
+                continue;
+            }
+            int x = pos_x + mask_x;
+            int y = pos_y + mask_y;
+            if (x < 0 || y < 0 || x >= DIGIT_WIDTH || y >= DIGIT_HEIGHT) {
+                return 0;
+            }
+            if ((*raw)[y][x] != letter) {
+                return 0;
+            }
+            if (clear) {
+                (*raw)[y][x] = ' ';
+            }
+        }
+    }
+    return 1;
+}
+
+static int parse_raw_digit(DigitDef* def, RawDigit* raw) {
+    memset(def, 0, sizeof(*def));
+  
+    int def_size = 0;
+  
+    for (int t = 0; t < TETRIMINO_COUNT; ++t) {
+        const char def_letter = s_tetrimino_defs[t].letter;
+        for (int r = 0; r < 4; ++r) {
+            const TetriminoMask* mask = &s_tetrimino_defs[t].rotations[r];
+      
+            // find first occupied mask position
+            int mask_start_x = 0;
+            int mask_start_y = 0;
+            for (; mask_start_y < TETRIMINO_MASK_SIZE; ++mask_start_y) {
+                for (; mask_start_x < TETRIMINO_MASK_SIZE; ++mask_start_x) {
+                    if (mask[mask_start_y][mask_start_x]) {
+                        break;
+                    }
+                }
+                if (mask_start_x < TETRIMINO_MASK_SIZE) {
+                    break;
+                }
+            }
+            APP_LOG(APP_LOG_LEVEL_INFO, "Processing %c:%d tetrimino with first block at %d:%d", def_letter, r, mask_start_x, mask_start_y);
+      
+            for (int pos_y = -TETRIMINO_MASK_SIZE; pos_y < DIGIT_HEIGHT + TETRIMINO_MASK_SIZE; ++pos_y) {
+                for (int pos_x = -TETRIMINO_MASK_SIZE; pos_x < DIGIT_WIDTH + TETRIMINO_MASK_SIZE; ++pos_x) {
+                    int x = pos_x + mask_start_x;
+                    int y = pos_y + mask_start_y;
+          
+                    if (x < 0 || y < 0 || x >= DIGIT_WIDTH || y >= DIGIT_HEIGHT) {
+                        continue;
+                    }
+          
+                    const char start_letter = (*raw)[y][x];
+                    if (def_letter != start_letter && def_letter != (start_letter - 'A' + 'a')) {
+                        continue;
+                    }
+          
+                    APP_LOG(APP_LOG_LEVEL_INFO, "Trying to match %c:%d tetrimino at %d:%d", def_letter, r, pos_x, pos_y);
+          
+                    if (match_mask(raw, mask, pos_x, pos_y, start_letter, 0)) {
+                        APP_LOG(APP_LOG_LEVEL_INFO, "Matched %c:%d tetrimino at %d:%d", def_letter, r, pos_x, pos_y);
+                        match_mask(raw, mask, pos_x, pos_y, start_letter, 1);            
+                        if (def_size < DIGIT_MAX_TETRIMINOS) {
+                            def->tetriminos[def_size].letter = def_letter;
+                            def->tetriminos[def_size].rotation = r;
+                            def->tetriminos[def_size].x = pos_x;
+                            def->tetriminos[def_size].y = pos_y;
+                            def_size += 1;
+                        }
+                    }
+                }
+            }
+        }    
+    }
+  
+    for (int y = 0; y < DIGIT_HEIGHT; ++y) {
+        for (int x = 0; x < DIGIT_WIDTH; ++x) {
+            if ((*raw)[y][x] != ' ') {
+                APP_LOG(APP_LOG_LEVEL_ERROR, "Can't parse raw digit, matched %d tetriminos:", def_size);
+                for (int i = 0; i < DIGIT_HEIGHT; ++i) {
+                    APP_LOG(APP_LOG_LEVEL_ERROR, "Row %02d: %.*s", i, DIGIT_WIDTH, (*raw)[i]);
+                }
+                return 0;
+            }      
+        }
+    }
+  
+    return def_size;
+}
+#endif
+
