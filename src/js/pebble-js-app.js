6496bc9b117246d409dd1bc8867350aaf7301620
vflashm <vflashm@gmail.com>
2015-05-25 15:23:20 -0400

    version 3.0 finalized

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -20,9 +20,9 @@ Pebble.addEventListener("appmessage",
 
 Pebble.addEventListener("showConfiguration",
     function(e) {
-        var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSdERwVTRLMUZOd2c';
+        var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSRHJZeURzeW96dG8';
         // debug url
-        var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSeldTZnFseHVBZGc';
+        // var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSeldTZnFseHVBZGc';
         var options = window.localStorage.getItem("options");
         if (options !== null) {
             options = escape(options);
