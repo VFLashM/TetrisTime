b3adf013c2c47b1f1eb640ec2e77ed4e04837e24
Valerii Lashmanov <vflashm@gmail.com>
2016-09-30 23:43:38 -0400

    url changed to github pages, version inc

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -20,9 +20,7 @@ Pebble.addEventListener("appmessage",
 
 Pebble.addEventListener("showConfiguration",
     function(e) {
-        var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSRHJZeURzeW96dG8';
-        // debug url
-        // var url = 'https://www.googledrive.com/host/0B1O5Onb__DjSeldTZnFseHVBZGc';
+        var url = 'http://vflashm.github.io/TetrisTime/';
         var options = window.localStorage.getItem("options");
         if (options !== null) {
             options = escape(options);
